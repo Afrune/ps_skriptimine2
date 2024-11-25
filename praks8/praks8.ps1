08d1e6d29a957da2d796f0a79b438be31fb12469
Mairo Künnap <mairo.kunnap@voco.ee>
2024-11-25 10:56:49 +0200

    Praktika 8 notepad running

---
+++ praks8/praks8.ps1
@@ -0,0 +1,14 @@
+﻿# Check Notepad Status
+while($true) {
+     # Check if Notepad is running
+     $proc = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
+
+     if ($proc) {
+         Write-Host "Notepad is running" -ForegroundColor Green
+     } else {
+         Write-Host "Notepad is not running" -ForegroundColor Red
+         break
+     }
+     # Wait for 2 seconds before checking again
+     Start-Sleep -Seconds 2
+}
