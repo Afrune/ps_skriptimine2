757d8e57c02439a057a36224a9d6f8a18a29ff86
Mairo Künnap <mairo.kunnap@voco.ee>
2024-11-25 11:38:18 +0200

    Do-While notepad

---
+++ praksit/pr9.ps1
@@ -0,0 +1,18 @@
+﻿
+$counter = 0
+do{
+    $counter++
+
+    $proc = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
+
+    if($proc){
+      Write-Host "Notepad is running" -ForegroundColor DarkGreen
+    }else{
+       Write-Host "Notepad is not running" -ForegroundColor DarkRed
+       break
+    }
+
+    Start-Sleep -Seconds 1
+}while($proc)
+
+Write-Host "$counter"
