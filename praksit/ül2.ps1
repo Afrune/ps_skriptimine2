b983b4256bd1ff73841b1ca98975efceeaf6900d
Mairo Künnap <mairo.kunnap@voco.ee>
2024-12-02 14:55:45 +0200

    Ülesanne 2 kasutaja kustutamine

---
+++ praksit/ül2.ps1
@@ -0,0 +1,13 @@
+﻿#Asks for first and lastname
+$ekfname =@(Read-Host "Sisesta oma eesnimi: ")
+$eklname =@(Read-Host "Sisesta oma perenimi: ")
+$kfname = $ekfname.ToLower()
+$klname = $eklname.ToLower()
+$kflname = "$kfname.$klname"
+#Checks if user exists and then removes it
+if(Get-LocalUser -Name $kflname -ErrorAction SilentlyContinue) {
+    Remove-LocalUser -Name $kflname
+    Write-Host "Given user $kflname has been removed."
+} else {
+     Write-Host "Given user $kflname doesnt exist."
+}
