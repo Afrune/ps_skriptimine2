6c78141237787531a29b52face67d322e017e6eb
Mairo Künnap <mairo.kunnap@voco.ee>
2024-12-02 14:42:41 +0200

    Ülesanne1 konto loomine

---
+++ praksit/ül1.ps1
@@ -0,0 +1,12 @@
+﻿$efname =@(Read-Host "Sisesta oma eesnimi: ")
+$elname =@(Read-Host "Sisesta oma perenimi: ")
+$fname = $efname.ToLower()
+$lname = $elname.ToLower()
+$flname ="$fname.$lname"
+$password = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+if(Get-LocalUser -Name $flname -ErrorAction SilentlyContinue) {
+   Write-Host "The user '$flname' already exists."
+} else {
+   New-LocalUser -Name $flname -Password $password -FullName "$efname $elname"
+   Write-Host "User '$flname' has been created successfully."
+}
